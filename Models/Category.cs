6f6e7c554db8182e5d7857765e2bbae6494f3bfa
longkuroko <longsteve.jobs@gmail.com>
2021-06-30 13:59:52 +0700

    add

---
+++ Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BigSchool.Models
+{
+    public class Category
+    {
+       
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+    }
+}
