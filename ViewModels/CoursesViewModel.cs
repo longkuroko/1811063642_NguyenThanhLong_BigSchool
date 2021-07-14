c1a6fb2fdacab0cdd03ef2c626e3b902172dd1c6
longkuroko <longsteve.jobs@gmail.com>
2021-07-14 15:47:47 +0700

    chấm bài

--- ViewModels/CoursesViewModel.cs
+++ ViewModels/CoursesViewModel.cs
@@ -8,6 +8,7 @@ namespace BigSchool.ViewModels
 {
     public class CoursesViewModel
     {
+        //cham bai
         public string dataSearch { get; set; }
         public IEnumerable<Course> UpcommingCourses { get; set; }
         public bool ShowAction { get; set; }
